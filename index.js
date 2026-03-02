b762e05b828c76464e9c634e14765e6b21eceae7
Nithish-raina <nithishraina0323@gmail.com>
2026-03-02 19:57:26 +0530

    feat: update package.json with detailed description, keywords, and repository info; add .npmignore file and remove test index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-/** This is a test file added to try out some snippets and gain
- * confidence in the code before adding it to the main codebase. This exists only for testing puposes
- */
-console.log(process.env.SHELL);
-console.log(process.env.PATH.split(":"));
